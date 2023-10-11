eca6ec4c4671c2d712c65b5c84ece301c5fca0a9
IgnacioNMiranda <ignacio.miranda@reign.cl>
2023-10-11 15:05:24 -0300

    chore: initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/jsx-key': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'warn',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/tailwind.css'
+
+function App() {
+  return (
+    <main>
+      <div className="h-screen">
+        <div className="flex justify-center items-center ">
+          <h1 className="text-red-700 text-xl">Time Travel Sparse Matrix</h1>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
