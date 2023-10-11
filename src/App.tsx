30f501ddc3d69c6081d9d74c898c2e248798f8ab
IgnacioNMiranda <ignacio.miranda@reign.cl>
2023-10-11 15:44:17 -0300

    feat: add cursor and background music

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
+import { BackgroundAudio } from './components/background-audio'
 import './styles/tailwind.css'
 
 function App() {
+  const [hasStart, setHasStart] = useState(false)
+
   return (
     <main>
       <div className="h-screen">
-        <div className="flex justify-center items-center ">
+        <div className="flex justify-center items-center flex-col gap-4">
           <h1 className="text-red-700 text-xl">Time Travel Sparse Matrix</h1>
+          {!hasStart && (
+            <button className="rounded px-4 py-2 bg-green-300" onClick={() => setHasStart(true)}>
+              Start :D
+            </button>
+          )}
         </div>
+
+        <BackgroundAudio play={hasStart} />
       </div>
     </main>
   )
---
+++ src/components/background-audio.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+
+export const BackgroundAudio = ({ play = false }: { play?: boolean }) => {
+  const [audio, setAudio] = useState<HTMLAudioElement>()
+  useEffect(() => {
+    if (play && !audio) {
+      const audioInstance = new Audio('/background-music.mp3')
+      audioInstance.play()
+      setAudio(audioInstance)
+    }
+  }, [play, audio])
+
+  return <></>
+}
